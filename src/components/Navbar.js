6012be7fc0d0bc1e98a934346365fdf1e07db446
= <shubhamdhapte007@gmail.com>
2021-12-13 11:29:45 +0530

    First

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
+import SearchIcon from "@material-ui/icons/Search";
+function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+  return (
+    <div className="Navbar">
+      <div className="leftSide">
+        <div className="links" id={showLinks ? "hidden" : ""}>
+          <a href="./home">Home</a>
+          <a href="./feedback">Feedback</a>
+          <a href="./aboutus">About Us</a>
+          <a href="./contact">Contact</a>
+          </div>
+        <button className="btn" onClick={() => setShowLinks(!showLinks)}>
+          <ReorderIcon />
+        </button>
+      </div>
+      
+      <div className="rightSide">
+        <input type="text" placeholder="Search.." />
+        <button>
+          <SearchIcon />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
